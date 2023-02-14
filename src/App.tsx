7ad63070b7626404ab47b29baa8a10a82e38a097
Nikson Rotondaro <niksonrotondaro1@gmail.com>
2023-02-14 08:00:16 +0000

    feat: add react router & implement

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import './App.css';
+import { Link, Outlet } from 'react-router-dom';
 
 function App() {
-  return <h1 className='text-2xl font-bold underline'>Hello world!</h1>;
+  return (
+    <h1 className='container mx-auto'>
+      <nav className='bg-gray-100 flex gap-4'>
+        <Link to='/invoices'>
+          <span>Invoice</span>
+        </Link>
+        <Link to='/expenses'>
+          <span>Expenses</span>
+        </Link>
+      </nav>
+      <Outlet></Outlet>
+    </h1>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Invoices from './components/invoices';
+import Expenses from './components/expenses';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}>
+        <Route path='/invoices' element={<Invoices />} />
+        <Route path='/expenses' element={<Expenses />} />
+        <Route
+          path='*'
+          element={
+            <main className='text-center'>
+              <p className='text-3xl'>There's nothing here !</p>
+            </main>
+          }
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
